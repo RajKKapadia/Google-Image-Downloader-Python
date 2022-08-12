28940a6f28eb220c4c84ce7928e158b0fa9f0eb2
RajKKapadia <raajforyou@gmail>
2022-08-12 09:01:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Google image scraper
+Hello friends, this is a Google image scraper repository. You can scrap use this to scrap images from Google image, provided the query string.
+
+## Youtube video
+Here is a humble try to explain this through a [video]().
+
+## Create environment
+It is strongly recommended to use a separate environment for this project to avoid any dependency issues with the existing packages.
+
+* create a new virtual environment, get more information [here](https://docs.python.org/3/tutorial/venv.html).
+* make sure you have Google chrome installed on your system
+* if you want to use this script on a hardware instance, you can use PyVirtualDisplay and wrap the driver into the virtual display, it will also need you to have Xvfb on you system as well.
+* install the require packages
+
+> ```pip install requirements.txt```
+
+## Usage
+There are two scripts `single_image_per_query.py` and `multiple_image_per_query.py`. 
+
+* to download single image per query use `single_image_per_query.py`
+* to download all the images or more than one image per query use `multiple_image_per_query.py`.
+
+# About me
+
+I am `Raj Kapadia`, I am passionate about `AI/ML/DL` and their use in different domains, I also love to build `chatbots` using `Google Dialogflow ES/CX`, I have backend development experience with Python[Flask], and NodeJS[Express] For any work, you can reach out to me at...
+
+* [LinkedIn](https://www.linkedin.com/in/rajkkapadia/)
+* [Fiverr](https://www.fiverr.com/rajkkapadia​)
+* [Upwork](https://www.upwork.com/freelancers/~0176aeacfcff7f1fc2)
+* [Youtube](https://www.youtube.com/channel/UCOT01XvBSj12xQsANtTeAcQ)
---
+++ single_image_per_query.py
@@ -0,0 +1,154 @@
+import time
+import base64
+from io import BytesIO
+import re
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+from PIL import Image
+
+cwd = os.getcwd()
+IMAGE_FOLDER = 'download'
+os.makedirs(
+    name=f'{cwd}/{IMAGE_FOLDER}',
+    exist_ok=True
+)
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(
+    service=service
+)
+
+SLEEP_TIME = 0.2
+
+def download_google_images(search_query: str, number_of_images: int) -> str:
+    '''Download google images with this function\n
+       Takes -> search_query, number_of_images\n
+       Returns -> None
+    '''
+    url = 'https://images.google.com/'
+
+    driver.get(
+        url=url
+    )
+
+    box = driver.find_element(
+        by=By.XPATH,
+        value="//input[contains(@class,'gLFyf gsfi')]"
+    )
+
+    box.send_keys(search_query)
+    box.send_keys(Keys.ENTER)
+    time.sleep(SLEEP_TIME)
+
+    img_results = driver.find_elements(
+        by=By.XPATH,
+        value="//img[contains(@class,'rg_i Q4LuWd')]"
+    )
+
+    total_images = len(img_results)
+
+    count = 0
+
+    for img_result in img_results:
+        try:
+            WebDriverWait(
+                driver,
+                15
+            ).until(
+                EC.element_to_be_clickable(
+                    img_result
+                )
+            )
+            img_result.click()
+            time.sleep(SLEEP_TIME)
+
+            actual_imgs = driver.find_elements(
+                by=By.XPATH,
+                value="//img[contains(@class,'n3VNCb')]"
+            )
+
+            src = ''
+
+            for actual_img in actual_imgs:
+                if 'https://encrypted' in actual_img.get_attribute('src'):
+                    pass
+                elif 'http' in actual_img.get_attribute('src'):
+                    src += actual_img.get_attribute('src')
+                    break
+                else:
+                    pass
+
+            for actual_img in actual_imgs:
+                if src == '' and 'base' in actual_img.get_attribute('src'):
+                    src += actual_img.get_attribute('src')
+
+            if 'https://' in src:
+                image_name = search_query.replace('/', ' ')
+                image_name = re.sub(pattern=" ", repl="_", string=image_name)
+                file_path = f'{IMAGE_FOLDER}/{count}_{image_name}.jpeg'
+                try:
+                    result = requests.get(src, allow_redirects=True, timeout=10)
+                    open(file_path, 'wb').write(result.content)
+                    img = Image.open(file_path)
+                    img = img.convert('RGB')
+                    img.save(file_path, 'JPEG')
+                    print('Image saved from https.')
+                    return file_path
+                except:
+                    print('Bad image.')
+                    try:
+                        os.unlink(file_path)
+                    except:
+                        pass
+                    count -= 1
+            else:
+                img_data = src.split(',')
+                image_name = search_query.replace('/', ' ')
+                image_name = re.sub(pattern=" ", repl="_", string=image_name)
+                file_path = f'{IMAGE_FOLDER}/{count}_{image_name}.jpeg'
+                try:
+                    img = Image.open(BytesIO(base64.b64decode(img_data[1])))
+                    img = img.convert('RGB')
+                    img.save(file_path, 'JPEG')
+                    print('Image saved from Base64.')
+                    return file_path
+                except:
+                    print('Bad image.')
+                    count -= 1
+        except ElementClickInterceptedException as e:
+            count -= 1
+            print(e)
+            print('Image is not clickable.')
+            driver.quit()
+
+        count += 1
+        if count >= total_images:
+            print('No more images to download.')
+            break
+        if count == number_of_images:
+            break
+
+tags = [
+    'Elon Musk',
+    'Tim Cook',
+    'Sundar Pichai',
+    'Steve Jobs'
+]
+
+for tag in tags:
+    file_path = download_google_images(
+        tag,
+        1
+    )
+    print(file_path)
+
+driver.quit()
